31117648b00ff1392c5d1323795fb9d5979516f5
Dhayana <contatodhayana@gmail.com>
2020-05-30 19:13:26 -0300

    mascara de telefone

---
+++ script.js
@@ -0,0 +1,20 @@
+const telefone = document.getElementById("telefone")
+const modal = document.getElementById("modal-container")
+
+const botaoFechar = document.getElementById("fechar-modal")
+const botaoAbrir = document.getElementById("abrir-modal")
+
+botaoFechar.addEventListener("click", () => modal.style.display='none')
+botaoAbrir.addEventListener("click", () => modal.style.display='block')
+telefone.addEventListener("input", (e) => e.target.value = mascaraTelefone(e.target.value))
+
+function mascaraTelefone(valor) {
+    let  retorno =  valor
+        .replace(/\D/g,'')
+        .replace(/(\d{2})(\d)/,'($1) $2')
+        .replace(/(\d{4})(\d)/,'$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/,'$1$2-$3')
+        .replace(/(-\d{4})\d+?$/,'$1')
+
+    return retorno
+}
